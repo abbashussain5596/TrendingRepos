dac34b10cfd8914e2c16bbc40e104dd12bfa461d
abbashussain <abbas.hussain5596@gmail.com>
2023-05-04 17:14:44 +0500

    Added repo Implementation for Success

--- app/src/main/java/com/tech/abbas/trendingrepos/data/repository/RepoRepositoryImpl.kt
+++ app/src/main/java/com/tech/abbas/trendingrepos/data/repository/RepoRepositoryImpl.kt
@@ -1,16 +1,20 @@
 package com.tech.abbas.trendingrepos.data.repository
 
+import com.tech.abbas.trendingrepos.data.mapper.toGithubRepo
 import com.tech.abbas.trendingrepos.data.remote.service.RepoService
 import com.tech.abbas.trendingrepos.domain.entity.GithubRepo
 import com.tech.abbas.trendingrepos.domain.repository.IRepoRepository
 import com.tech.abbas.trendingrepos.domain.util.Result
+import com.tech.abbas.trendingrepos.domain.util.asResult
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 internal class RepoRepositoryImpl @Inject constructor(
     private val service:RepoService
 ):IRepoRepository {
-    override fun getRepoList(): Flow<Result<List<GithubRepo>>> {
-        TODO("Not yet implemented")
-    }
+    override fun getRepoList(): Flow<Result<List<GithubRepo>>> = flow {
+        val githubRepos = service.getRepoList()
+        emit(githubRepos.items.toGithubRepo())
+    }.asResult()
 }
