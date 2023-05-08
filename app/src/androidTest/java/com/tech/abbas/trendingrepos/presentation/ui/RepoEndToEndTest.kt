ebb3811206f1814179b2080c5d9828cce1d2aeb9
abbashussain <abbas.hussain5596@gmail.com>
2023-05-08 18:11:36 +0500

    TODO: Add End to End test with Mock Servers with Success test

--- app/src/androidTest/java/com/tech/abbas/trendingrepos/presentation/ui/RepoEndToEndTest.kt
+++ app/src/androidTest/java/com/tech/abbas/trendingrepos/presentation/ui/RepoEndToEndTest.kt
@@ -1,5 +1,6 @@
 package com.tech.abbas.trendingrepos.presentation.ui
 
+import androidx.compose.ui.test.onNodeWithTag
 import com.tech.abbas.trendingrepos.base.BaseTest
 import com.tech.abbas.trendingrepos.di.NetworkModule
 import com.tech.abbas.trendingrepos.di.RepoModule
@@ -9,6 +10,7 @@ import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
 import kotlinx.coroutines.flow.MutableStateFlow
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito
@@ -36,36 +38,12 @@ internal class RepoEndToEndTest : BaseTest() {
         }
 
     }
-    //TODO: Add End to End test with Mock Servers
 
-//    @Test
-//    fun repoListSuccessFlowTest() {
-//        Mockito.`when`(repoScreenViewModel.uiState).thenReturn(
-//            (MutableStateFlow(ReposUIState.Idle))
-//        )
-//        Mockito.`when`(repoScreenViewModel.expandedCardIdsList).thenReturn(
-//            (MutableStateFlow(listOf()))
-//        )
-//        initSuccessMockServerDispatcher()
-//        composeTestRule.onNodeWithTag(RepoListScreenKtTest.NETWORK_ERROR).assertDoesNotExist()
-////        composeTestRule.onNodeWithTag(RepoListScreenKtTest.REPO_LIST).assertDoesNotExist()
-//        composeTestRule.onNodeWithTag(RepoListScreenKtTest.REPO_LIST).assertIsDisplayed()
-//
-//    }
-//    @Test
-//    fun whenRepoListIsInErrorStateAndClickOnRetry() {
-//
-//        initSuccessMockServerDispatcher()
-//
-//        composeTestRule.onNodeWithTag(RETRY_BUTTON)
-//            .performClick()
-//
-//        val loaderCount =
-//            composeTestRule.onAllNodes(hasTestTag(LOADING)).fetchSemanticsNodes().size
-//        assert(loaderCount == 9)
-//
-//        composeTestRule.waitUntilExists(hasTestTag(REPO_LIST))
-//
-//    }
+    //TODO: Add End to End test with Mock Servers
+    @Test
+    fun repoListSuccessFlowTest() {
+        initSuccessMockServerDispatcher()
+    }
+
 
 }
